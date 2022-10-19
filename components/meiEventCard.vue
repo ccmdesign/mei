5d11438a98dbcea7329359f6f711c58fc7df4679
Claudio <claudio@ccmdesign.ca>
2022-10-19 14:50:58 -0700

    first stab at events section on home

---
+++ components/meiEventCard.vue
@@ -0,0 +1,39 @@
+<template>
+  <base-card class="mei-event-card"
+    tagline="Tue., Apr. 19, 2022 | Duration: 1h30"
+    :videoUrl="videoUrl"
+    hideAction
+  >
+  <template #image>
+    <div class="frame">
+      <iframe width="560" height="315" :src="videoUrl" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </div>
+  </template>
+  </base-card>
+</template>
+
+<script setup>
+import { toRefs } from 'vue'
+
+const props = defineProps({
+  videoUrl: {
+    type: String,
+    default: "#"
+  },
+});
+const { videoUrl } = toRefs(props)
+</script>
+
+<style lang="scss" scoped>
+.mei-event-card {
+  --card-border: 0;
+  --card-border-radius: 0;
+  --card-frame-d: 16; 
+  --card-frame-n: 9;
+  --card-padding: 0;
+}
+
+.mei-event-card :deep(.base-card__title) { line-height: 1.5; }
+.mei-event-card :deep(.base-card__title-link) { color: var(--primary-color); }
+
+</style>
--- components/meiEvents.vue
+++ components/meiEvents.vue
@@ -1,22 +1,46 @@
 <template>
   <base-section>
     <center-l size="wide">
-      <stack-l>
-        <h2>Mei Events</h2>
-        <mei-card-wide />
-        <mei-card-wide />
-        <mei-card-wide />
+      <stack-l space="var(--s2)">
+        <h2 class="color:primary">Events</h2>
+        
+        <tab-bar :options="tabOptions" />
+
+        <div class="grid">
+          <mei-event-card 
+            heading="Vision or Mirage: Saudi Arabia at the Crossroads"
+            videoUrl="https://www.youtube.com/embed/_Us_QodyTio"
+          />
+          <mei-event-card 
+            heading="Networked Refugees: Palestinian Reciprocity and Remittances in the Digital Age"
+            videoUrl="https://www.youtube.com/embed/NSdMgyYfufw"
+          >
+          </mei-event-card>
+        </div>
+        <div class="text-align:center">
+          <nuxt-link to="/events/" class="button" data-color="primary" data-visual="primary">View all MEI events</nuxt-link>
+        </div>
       </stack-l>
     </center-l>
-    
-    
   </base-section>
 </template>
 
 <script setup>
-import meiCardWide from '@/components/meiCardWide.vue';
+import meiEventCard from '@/components/meiEventCard.vue';
+import tabBar from '@/components/tabBar.vue';
+
+const tabOptions = [
+  {
+    'label': 'Upcoming Events',
+    'value': 'upcoming'
+  },
+  {
+    'label': 'Past Events',
+    'value': 'past'
+  }
+]
 </script>
 
 <style lang="scss" scoped>
-
+.grid { grid-gap: var(--s2); }
 </style>
