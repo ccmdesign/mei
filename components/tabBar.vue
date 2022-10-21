0d70b8f26ae803004334c4aeb5f2915db5cf24af
Claudio <claudio@ccmdesign.ca>
2022-10-20 19:05:43 -0700

    DS-CANDIDATE - TabBar

--- components/tabBar.vue
+++ components/tabBar.vue
@@ -2,9 +2,11 @@
 
 <template>
   <section class="tab-bar">
-    <slot>
-      <base-button v-for="i in options" visual="secondary" color="base" :value="i.value">{{i.label}}</base-button>
-    </slot>
+    <label :for="i.value" v-for="i in options" class="tab-bar__item">
+      <input :id="i.value" type="radio" name="tabs" :value="i.value" :checked="i.defaultOption">
+      <span class="tab-bar__button button" data-visual="unstyled" data-color="primary">{{i.label}}</span>
+    </label>
+    
   </section>
 </template>
 
@@ -18,8 +20,9 @@ const props = defineProps({
   options: {
     type: Array,
     default: [
-      { label: 'Label Example', value: 'Value Example'},
-      { label: 'Label Example', value: 'Value Example'}
+      { label: 'Label Example', value: 'value-example-1', defaultOption: 'true'},
+      { label: 'Label Example', value: 'value-example-2'},
+      { label: 'Label Example', value: 'value-example-3'}
     ]
   },
 });
@@ -27,11 +30,35 @@ const { options } = toRefs(props)
 </script>
 
 <style lang="scss" scoped>
-.tab-bar > *:first-of-type  { border-radius: var(--button-border-radius) 0 0 var(--button-border-radius); }
 
-.tab-bar > * + *  { 
-  border-radius: 0; 
-  margin-left: -2px;
+.tab-bar {
+  display: flex;
+  justify-content: center;
+  border-bottom: 1px solid hsla(var(--base-hsl), .2);
 }
-.tab-bar > *:last-of-type  { border-radius: 0 var(--button-border-radius) var(--button-border-radius) 0; }
+
+.tab-bar__button { 
+  --button-border-radius: 0;
+  --button-padding-inline: var(--s1);   
+  --button-font-weight: 400;
+}
+
+.tab-bar__item input[type="radio"] {
+  display: none;
+}
+
+input:checked + .tab-bar__button  { 
+  --button-font-weight: 700;
+  position: relative;
+}
+
+input:checked + .tab-bar__button:after  {
+  content: "";
+  width: calc(100% + 4px);
+  height: 3px;
+  background-color: currentColor;
+  position: absolute;
+  left: -2px;
+  bottom: -4px;
+}
 </style>
