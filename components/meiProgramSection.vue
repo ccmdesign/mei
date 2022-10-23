5f5595cc50ef4b90edcbeb067a2e8f163785f3a1
Claudio <claudio@ccmdesign.ca>
2022-10-23 09:46:15 -0700

    programs page

---
+++ components/meiProgramSection.vue
@@ -0,0 +1,47 @@
+<template>
+  <div :class="content.texture ? 'mei-texture-bg | background-color:primary-03' : ''">
+    <base-Section class="program-panel" size="xl" color="transparent">
+      <center-l size="wide">
+        <div class="grid" :inverted="content.inverted">
+          <stack-l space="var(--s1)" class="program-panel__content">
+            <h3 class="color:primary">{{content.title}}</h3>
+            <p>{{content.description}}</p>
+            <div>
+              <!-- Implement new Button -->
+              <base-button color="primary" visual="primary">More</base-button>
+            </div>
+          </stack-l>
+          <figure class="program-panel__image | padding:s2">
+            <img class="center:all" :src="content.imageUrl" alt="">
+          </figure>
+        </div>
+      </center-l>
+    </base-Section>
+  </div>
+</template>
+
+<script setup>
+import { toRefs } from 'vue';
+
+const props = defineProps({
+  content: {
+    type: Object,
+    default: {}
+  },
+});
+
+const { inverted, content } = toRefs(props)
+</script>
+
+<style lang="scss" scoped>
+.grid[inverted] .program-panel__image { order: -1; }
+
+@media (max-width: 40em) {
+  .program-panel__image { order: -1; }
+}
+
+@media (min-width: 40em) {
+  .grid { grid-gap: var(--s4); }
+}
+
+</style>
---
+++ pages/programs.vue
@@ -0,0 +1,40 @@
+<template>
+  <article>
+    <mei-hero
+      background="../assets/images/programs-hero.png"
+      title="Programs"
+      description="The Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam nesciunt nostrum deleniti reprehenderit delectus facere quibusdam aliquam temporibus numquam, dolore error hic dolorem iste nobis voluptates tenetur et consectetur omnis."
+    />
+
+    <mei-program-section v-for="i in programData" :content='i'/>
+  </article>
+</template>
+
+<script setup>
+
+const programData = [
+  {
+    title: 'Emirates Leadership Initiative',
+    description: 'The deadline to apply for the Spring 2023 Kuwait Visiting Research Fellowship has passed. Applications for this position are no longer being accepted at this time.',
+    imageUrl: '../assets/images/emirates-logo.png'
+  },
+  {
+    title: 'Kuwait Program',
+    description: 'The deadline to apply for the Spring 2023 Kuwait Visiting Research Fellowship has passed. Applications for this position are no longer being accepted at this time.',
+    imageUrl: '../assets/images/kwait-logo.png',
+    inverted: true, 
+    texture: true
+  },
+  {
+    title: 'Tunisia Program',
+    description: 'The deadline to apply for the Spring 2023 Kuwait Visiting Research Fellowship has passed. Applications for this position are no longer being accepted at this time.',
+    imageUrl: '../assets/images/tunisia-logo.png'
+  }
+]
+
+</script>
+
+<style lang="scss" scoped>
+
+
+</style>
