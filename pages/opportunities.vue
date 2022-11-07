6e6d4cef891b9deab031b89b9998dcc91871c3bf
Seidgy Amano <seidgy@gmail.com>
2022-11-07 18:08:06 -0300

    MEI-27 Opportunities page

--- pages/opportunities.vue
+++ pages/opportunities.vue
@@ -1,7 +1,7 @@
 <template>
   <article>
     <mei-hero
-      background="../assets/images/publications-hero.png"
+      background="../assets/images/opportunities-hero.jpg"
       title="Opportunities"
       description="The Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam
       nesciunt nostrum deleniti reprehenderit delectus facere quibusdam
@@ -10,15 +10,12 @@
     />
     
     <base-section color="transparent">
-      <center-l size="wide">
-        <stack-l>
-          <tab-bar :options="tabs" />
-          <mei-opportunity-group/>
-          <mei-opportunity-group inverted/>
-          <mei-opportunity-group/>
-          
-        </stack-l>
-      </center-l>
+      <stack-l>
+        <tab-bar :options="tabs" />
+        <mei-opportunity-group :content="opportunities[0]" ellipsisClass="bottom-right"/>
+        <mei-opportunity-group :content="opportunities[1]" ellipsisClass="top-left" inverted :gradient="true" :pattern="true" />
+        <mei-opportunity-group :content="opportunities[2]" ellipsisClass="top-right"/>
+      </stack-l>0
     </base-section>
     
   </article>
@@ -27,9 +24,120 @@
 <script setup>
 import tabBar from '@/components/tabBar.vue';
 const tabs = [
-  { label: 'Students', value: 'students', defaultOption: 'true'},
-  { label: 'Faculty', value: 'faculty'},
-  { label: 'Outside Scholars', value: 'outside-scholars'}
+  { label: 'Students', value: '#students', defaultOption: 'true'},
+  { label: 'Faculty', value: '#faculty'},
+  { label: 'Outside Scholars', value: '#outside-scholars'}
+]
+
+const opportunities = [
+  {
+    imageUrl: '../assets/images/opportunities1.jpg',
+    imageLabel: 'Opportunities for Harvard Students',
+    heading: 'Opportunities for Harvard Students',
+    excerpt: 'The deadline to apply for the Spring 2023 Kuwait Visiting Research Fellowship has passed. Applications for this position are no longer being accepted at this time.',
+    groups: [
+      {
+        heading: 'J-term course',
+        excerpt: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis quod possimus repudiandae aliquid error. Illo id error saepe quis, mollitia in sapiente sit neque aspernatur porro vel, facilis enim pariatur.',
+        links: [
+          {
+            title: 'Current Syllabus',
+            url: '#'
+          },
+          {
+            title: 'Photos from past course',
+            url: '#'
+          },
+        ]
+      },
+      {
+        heading: 'Other Student Opportunities',
+        excerpt: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis quod possimus repudiandae aliquid error. Illo id error saepe quis, mollitia in sapiente sit neque aspernatur porro vel, facilis enim pariatur.',
+        links: [
+          {
+            title: 'Current Syllabus',
+            url: '#'
+          },
+          {
+            title: 'Photos from past course',
+            url: '#'
+          },
+        ]
+      }
+    ]
+  },
+  {
+    imageUrl: '../assets/images/opportunities2.jpg',
+    imageLabel: 'Research Grants for Harvard Faculty',
+    heading: 'Research Grants for Harvard Faculty',
+    excerpt: 'The deadline to apply for the Spring 2023 Kuwait Visiting Research Fellowship has passed. Applications for this position are no longer being accepted at this time. ',
+    groups: [
+      {
+        heading: 'Emirates Leadership Initiative Research Fellowship',
+        excerpt: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis quod possimus repudiandae aliquid error. Illo id error saepe quis, mollitia in sapiente sit neque aspernatur porro vel, facilis enim pariatur.',
+        links: [
+          {
+            title: 'Current Grants',
+            url: '#'
+          },
+          {
+            title: 'How to apply',
+            url: '#'
+          },
+        ]
+      },
+      {
+        heading: 'Kuwait Grants',
+        excerpt: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis quod possimus repudiandae aliquid error. Illo id error saepe quis, mollitia in sapiente sit neque aspernatur porro vel, facilis enim pariatur.',
+        links: [
+          {
+            title: 'Current Grants',
+            url: '#'
+          },
+          {
+            title: 'How to apply',
+            url: '#'
+          },
+        ]
+      }
+    ]
+  },
+  {
+    imageUrl: '../assets/images/opportunities3.jpg',
+    imageLabel: 'Opportunities for Scholars',
+    heading: 'Opportunities for Scholars',
+    excerpt: 'The deadline to apply for the Spring 2023 Kuwait Visiting Research Fellowship has passed. Applications for this position are no longer being accepted at this time. ',
+    groups: [
+      {
+        heading: 'Emirates Leadership Initiative Research Fellowship',
+        excerpt: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis quod possimus repudiandae aliquid error. Illo id error saepe quis, mollitia in sapiente sit neque aspernatur porro vel, facilis enim pariatur.',
+        links: [
+          {
+            title: 'Current Grants',
+            url: '#'
+          },
+          {
+            title: 'How to apply',
+            url: '#'
+          },
+        ]
+      },
+      {
+        heading: 'Kuwait Grants',
+        excerpt: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis quod possimus repudiandae aliquid error. Illo id error saepe quis, mollitia in sapiente sit neque aspernatur porro vel, facilis enim pariatur.',
+        links: [
+          {
+            title: 'Current Grants',
+            url: '#'
+          },
+          {
+            title: 'How to apply',
+            url: '#'
+          },
+        ]
+      }
+    ]
+  },
 ]
 </script>
 
