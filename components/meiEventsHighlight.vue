774d0e27d67cad4c81db528e8c1ccbbf7b4f1e69
Lívia Simões <livias.simoes36@gmail.com>
2022-11-28 15:07:56 -0300

    Events Highlight component

---
+++ components/meiEventsHighlight.vue
@@ -0,0 +1,56 @@
+<template>
+  <base-section size="m" color="transparent">
+    <center-l size="wide">
+      <stack-l space="var(--s3)">
+        <h2 class="color:primary">Events</h2>
+        <mei-events :data="[upcomingEvents, pastEvents]" showHighlights moreUrl="/events/" />
+      </stack-l>
+    </center-l>
+  </base-section>
+</template>
+
+<script setup>
+const UPCOMING_MAX_EVENTS = 3; // FIXME: Máximo de eventos futuros sendo mostrados na tela.
+const PAST_MAX_EVENTS = 4; // FIXME: Máximo de eventos passados sendo mostrados na tela.
+
+const events = await queryContent("event").find();
+const today = new Date();
+
+const upcomingEvents = {
+  heading: 'Upcoming Events',
+  list: reactive([]),
+  highlights: reactive([])
+};
+
+const pastEvents = {
+  heading: 'Past Events',
+  list: reactive([])
+};
+
+let pos = 0;
+
+// Toda a lógica abaixo pressupõe que os eventos virão ordenados do content
+// pela data de início (start_date) do mais recente para o mais antigo.
+//
+do {
+  if (events.length <= pos) break; // Se chegar no fim da lista de eventos.
+  const event = events[pos];
+  const eventDate = new Date(event.start_date);
+
+  if (eventDate >= today) {
+    if (!!upcomingEvents.highlights) {
+      upcomingEvents.highlights = event; // FIXME: O primeiro evento seria sempre o em destaque.
+
+    } else if (upcomingEvents.list.length < UPCOMING_MAX_EVENTS) {
+      upcomingEvents.list.push(event);
+    }
+
+  } else {
+    pastEvents.list = events.slice(pos, PAST_MAX_EVENTS);
+  }
+
+  pos += 1;
+
+} while (pastEvents.list.length < PAST_MAX_EVENTS);
+
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -12,7 +12,7 @@
 
     <mei-about-highlight class="mei-texture-bg"/>
 
-    <mei-events :data="[upcomingEvents, pastEvents]" showHighlights moreUrl="/events/" />
+    <mei-events-highlight />
 
     <mei-people-highlight class="mei-texture-bg" :options="peopleData" />
     
@@ -25,50 +25,6 @@
 </template>
 
 <script setup>
-const UPCOMING_MAX_EVENTS = 3; // FIXME: Máximo de eventos futuros sendo mostrados na tela.
-const PAST_MAX_EVENTS = 4; // FIXME: Máximo de eventos passados sendo mostrados na tela.
-
-const events = await queryContent("event").find();
-const today = new Date();
-
-const upcomingEvents = {
-  heading: 'Upcoming Events',
-  list: reactive([]),
-  highlights: reactive([])
-};
-
-const pastEvents = {
-  heading: 'Past Events',
-  list: reactive([])
-};
-
-let pos = 0;
-
-// Toda a lógica abaixo pressupõe que os eventos virão ordenados do content
-// pela data de início (start_date) do mais recente para o mais antigo.
-//
-do {
-  if (events.length <= pos) break; // Se chegar no fim da lista de eventos.
-  const event = events[pos];
-  const eventDate = new Date(event.start_date);
-
-  if (eventDate >= today) {
-    if (!!upcomingEvents.highlights) {
-      upcomingEvents.highlights = event; // FIXME: O primeiro evento seria sempre o em destaque.
-
-    } else if (upcomingEvents.list.length < UPCOMING_MAX_EVENTS) {
-      upcomingEvents.list.push(event);
-    }
-
-  } else {
-    pastEvents.list = events.slice(pos, PAST_MAX_EVENTS);
-  }
-
-  pos += 1;
-
-} while (pastEvents.list.length < PAST_MAX_EVENTS);
-
-
 // not working because mei-people-highlight props are not set up
 const peopleData = {
   tabs: [
