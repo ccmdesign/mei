d37daec3c6a4994bcc3a1e2b91817007eb431e77
Lívia Simões <livias.simoes36@gmail.com>
2022-12-06 20:26:30 -0300

    Fix People Highlight on People page

--- components/meiPeopleWide.vue
+++ components/meiPeopleWide.vue
@@ -1,116 +1,98 @@
 <template>
   <div>
-    <person-card
-      class="mei-people-wide | mei-texture-bg"
-      horizontal
-      highlight
-      :shadow="false"
-      actionLabel="More"
-      :fullClick="false"
-      :heading="data.name"
-      :htmlExcerpt="data.biography"
-      :imageUrl="data.avatar.url"
-    >
-
-    <template #headings>
+    <person-card class="mei-people-wide | mei-texture-bg" horizontal clamp=5 :shadow="false" actionLabel="More"
+      :fullClick="false" :heading="name" :htmlExcerpt="biography" :imageUrl="imageUrl">
+      <template #headings>
         <h2 class="mei-people-wide__title">
-          <a class="color-primary" :href="data.url" target="_blank">
-            {{ data.name }}
+          <a class="color-primary" :href="url" target="_blank">
+            {{ name }}
           </a>
-          <a class="mei-people-wide__title-email icon" :href="`mailto:${data.email}`">email</a>
+          <a class="mei-people-wide__title-email icon" :href="`mailto:${email}`">email</a>
         </h2>
 
-        <h4 class="mei-people-wide__tagline">{{ data.belfer_role[0] }}</h4>
+        <h4 class="mei-people-wide__tagline">{{ role }}</h4>
 
         <p>Biography</p>
       </template>
 
       <template #action>
-        <!-- FIXME: Essa template não está sendo renderizada. -->
-        <base-button
-          el="a"
-          :href="data.url"
-          color="primary"
-          visual="primary"
-          >
+        <base-button el="a" color="primary" visual="primary" :href="url">
           More
-          </base-button
-        >
+        </base-button>
       </template>
     </person-card>
   </div>
 </template>
 
 <script setup>
-  // import { toRefs } from 'vue';
+const props = defineProps({
+  url: {
+    type: String,
+    default: ''
+  },
+  role: {
+    type: String,
+    default: ''
+  },
+  email: {
+    type: String,
+    default: ''
+  },
+  name: {
+    type: String,
+    default: ''
+  },
+  biography: {
+    type: String,
+    default: ''
+  },
+  imageUrl: {
+    type: String,
+    default: ''
+  }
+});
 
-  /*--- Highlight. ----*/
-  // FIXME: Pegar somente uma (ou a mais recente) pessoa em destaque.
-
-  const data = await queryContent("person")
-    .where({ belfer_role: ["Staff"] })
-    .findOne();
-
-  // FIXME: Trazer a foto.
-
-  // FIXME: A "tagline" no exemplo aparece como "Faculty Director". Esse não é um papel no
-  // campo "belfer_role". De onde ele vem?
-  // Corrigir o tagline para o valor correto.
-
-  // const props = defineProps({
-  //   data: {
-  //     type: Object,
-  //     default: {
-  //       name: "Tarek Masoud",
-  //       tagline: "Faculty Director", // FIXME: Ver acima!
-  //       biography: "Tarek Masoud is the Ford Foundation Professor of Democracy and Governance at Harvard University's John F. Kennedy School of Government. He is the co-Editor of the Journal of Democracy of the National Endowment for Democracy and serves as the Faculty Director of the Kennedy School's Middle East Initiative ...",
-  //       url: '',
-  //       email: 'example@email.com',
-  //     }
-  //   }
-  // });
-
-  // const { data } = toRefs(props);
+const { url, role, email, name, biography, imageUrl } = toRefs(props);
 </script>
 
+
 <style lang="scss" scoped>
-  .mei-people-wide {
-    --card-padding: var(--s2);
-    --card-image-padding: var(--s2);
-    --card-hover-border: none;
-    --card-border: none;
-    --card-border-radius: none;
-    --card-hover-shadow: none;
-    --card-gap: var(--s2);
-    --card-vertical-space: var(--s0);
-    --card-headings-alignment: left;
-    --card-action-hsl: var(--primary-hsl);
-  }
+.mei-people-wide {
+  --card-padding: var(--s2);
+  --card-image-padding: var(--s2);
+  --card-hover-border: none;
+  --card-border: none;
+  --card-border-radius: none;
+  --card-hover-shadow: none;
+  --card-gap: var(--s2);
+  --card-vertical-space: var(--s0);
+  --card-headings-alignment: left;
+  --card-action-hsl: var(--primary-hsl);
+}
 
-  .mei-people-wide :deep(.circle) {
-    padding: var(
-      --card-image-padding
-    ); // ToDo: Include this config in the person card?
-    margin-left: var(--s2);
-    width: 320px;
-    height: 320px;
-  }
+.mei-people-wide :deep(.circle) {
+  padding: var(--card-image-padding); // ToDo: Include this config in the person card?
+  margin-left: var(--s2);
+  width: 320px;
+  height: 320px;
+}
 
-  .mei-people-wide :deep(h2) {
-    color: var(--primary-color);
-    font-size: 1.75em;
-  }
-  .mei-people-wide :deep(h4) {
-    font-size: 1.25em;
-    font-weight: bold;
-  }
+.mei-people-wide :deep(h2) {
+  color: var(--primary-color);
+  font-size: 1.75em;
+}
 
-  .mei-people-wide__title :deep(a) {
-    text-decoration: none;
-  }
+.mei-people-wide :deep(h4) {
+  font-size: 1.25em;
+  font-weight: bold;
+}
 
-  .mei-people-wide__title-email {
-    margin-left: var(--s0);
-    vertical-align: middle;
-  }
+.mei-people-wide__title :deep(a) {
+  text-decoration: none;
+}
+
+.mei-people-wide__title-email {
+  margin-left: var(--s0);
+  vertical-align: middle;
+}
 </style>
--- pages/people.vue
+++ pages/people.vue
@@ -6,16 +6,43 @@
       description="The Middle East Initiative at Harvard Kennedy School is dedicated to advancing public policy in the Middle East by convening the world's foremost academics and policy experts, developing the next generation of leaders, and promoting community engagement on campus and in the region."
     />
 
-    <base-section size="s" class="margin-top:s3">
+    <base-section size="s" class="margin-top:s3" v-if="!!person">
       <center-l size="wide">
-        <mei-people-wide />
+        <mei-people-wide 
+        :url="person.url"
+        :role="(person.display_title || person.belfer_role[0])"
+        :email="person.email"
+        :name="person.name"
+        :biography="person.biography"
+        :imageUrl="person.avatar.url"
+        />
       </center-l>
     </base-section>
 
-    <base-section size="">
+    <base-section>
       <center-l size="wide">
         <mei-people />
       </center-l>
     </base-section>
   </article>
 </template>
+
+<script setup>
+const data = await queryContent('highlight')
+  .where({content_type: 'person'})
+  .find();
+
+let person = undefined;
+
+const _sort = (a, b) => {
+  if (a.changed < b.changed) return -1;
+  if (a.changed > b.changed) return 1;
+
+  return 0
+};
+
+// Get the most recently changed person set as highlight.
+if (data.length > 0) {
+  person = reactive(data.sort(_sort)[0]);
+}
+</script>
