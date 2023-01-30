b734f30a46abeb2e874da8f068eccd193d9f92fe
Lívia Simões <livias.simoes36@gmail.com>
2023-01-30 18:38:53 -0300

    Add default layout for  iframe changing url parent

---
+++ layouts/default.vue
@@ -0,0 +1,26 @@
+<template>
+    <a class="production-link" ref="productionlink" href="#" target="_top">Replace parent url</a>
+  <slot />
+</template>
+
+<script setup>
+import { ref, watch } from 'vue'
+import { useRoute } from "vue-router"
+const route = useRoute();
+const productionlink = ref(null);
+// URL de produção.
+// Caution: Não pode ter a barra no final.
+// FIXME: Pegar essa URL de uma variável e bloquear para s ó funcionar em produção. 
+const baseLink = 'https://www.belfercenter.org/mei-website-iframe-draft'; 
+
+watch(route, (to) => {
+  productionlink.value.href = baseLink + to.fullPath;
+  productionlink.value.click();
+})
+</script>
+
+<style lang="scss">
+  .production-link {
+    display: none;
+  }
+</style>
