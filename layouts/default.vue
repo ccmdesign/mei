48d2d3fcf29c694f258538348cd42d37188e3a20
Lívia Simões <livias.simoes36@gmail.com>
2023-02-09 17:55:29 -0300

    Remove iframe height

--- layouts/default.vue
+++ layouts/default.vue
@@ -10,40 +10,13 @@ const route = useRoute();
 const productionlink = ref(null);
 // URL de produção.
 // Caution: Não pode ter a barra no final.
-// FIXME: Pegar essa URL de uma variável e bloquear para s ó funcionar em produção. 
-const baseLink = 'http://127.0.0.1:8887'; 
+// FIXME: Pegar essa URL de uma variável e bloquear para só funcionar em produção. 
+const baseLink = 'https://mei-hksbelfer.pantheonsite.io/mei'; 
 
 watch(route, (to) => {
   productionlink.value.href = baseLink + to.fullPath;
   productionlink.value.click();
 })
-
-document.addEventListener("DOMContentLoaded", function() {
-    window.setTimeout(function() {    
-        const imgs = document.images;
-        let len = imgs.length;
-        let counter = 0;
-
-        [].forEach.call( imgs, function( img ) {
-            if(img.complete)
-            incrementCounter();
-            else
-            img.addEventListener( 'load', incrementCounter, false );
-        } );
-
-        function incrementCounter() {
-            counter++;
-            if ( counter === len ) {
-                const body = document.body;
-                const html = document.documentElement;
-                let height = 0;
-                height = Math.max( body.scrollHeight, body.offsetHeight, html.offsetHeight );
-                window.parent.postMessage((height+50)+'px', '*');
-            }
-        }
-    }, 500);
-});
-
 </script>
 
 <style lang="scss">
