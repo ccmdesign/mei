97459f7ad3edcb91f219610b106b63535a2497b7
Lívia Simões <livias.simoes36@gmail.com>
2023-05-19 16:49:04 -0300

    Add Vue error page

---
+++ error.vue
@@ -0,0 +1,25 @@
+<template>
+  <article>
+    <mei-hero 
+      background="../assets/images/home-bg.png"
+      title="Middle East Initiative"
+      description="The Middle East Initiative at Harvard Kennedy School is dedicated to advancing public policy in the Middle East by convening the world's foremost academics and policy experts, developing the next generation of leaders, and promoting community engagement on campus and in the region."
+    />
+    <base-section>
+    <center-l size="wide">
+      <stack-l space="var(--s2)" class="margin-bottom:s2">
+        <h2 class="color:primary">Page not found</h2>
+        <p>Sorry, we couldn't find the page you were looking for.</p>
+      </stack-l>
+    </center-l>
+    </base-section>
+  </article>  
+</template>
+
+<style scoped>
+.cover { --height: 30;}
+
+.people-index { 
+  --itemWidth: 100px; 
+}
+</style>
